c358a9d016004bcdfa40be7a75d8deb20b766f18
tristan <thuckabee.work@gmail.com>
2023-12-05 16:39:03 -0600

    outline made

---
+++ week-1/day-1/scratch.js
@@ -0,0 +1,15 @@
+// Printing to the Console
+
+// Comments
+
+// Variable Declarations
+
+// Strings
+
+// Math Operators
+
+// Booleans
+  // Comparison Operators
+  // De Morgans Law
+
+// Shorthand Operations
