9a7b8bb567bfc5d748548ce5d9f906587693d42d
tristan <thuckabee.work@gmail.com>
2024-01-23 21:22:39 -0600

    west done

--- week-7/day-2/calculator.js
+++ week-7/day-2/calculator.js
@@ -7,3 +7,18 @@ const divide   = (num1, num2) => num1 / num2;
 
 
 // Exports
+// module.exports = operations;
+
+// module.exports = {
+//   add : add,
+//   subtract,
+//   operations
+// }
+
+// module.exports.add = add;
+// module.exports.subtract = subtract;
+// module.exports['operations'] = operations;
+
+exports.add = add;
+exports.subtract = subtract;
+exports.operations = operations;
--- week-7/day-2/scratch.js
+++ week-7/day-2/scratch.js
@@ -1,1 +1,14 @@
 // Imports
+// const calculator = require('./calculator.js');
+const { add, subtract, operations } = require('./calculator.js');
+const name = require('./file01/hello.js')
+//../day-1/scratch.js
+
+// if (x) return 12;
+// if (y) return 12;
+// // ...
+// // ...
+// if (x || y) return 12
+
+// console.log(calculator.add(2, 2));
+console.log(add(2, 2));
