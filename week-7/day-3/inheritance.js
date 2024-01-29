e566d2b3e71deb901bb5192c4fc817b642e262d7
tristan <thuckabee.work@gmail.com>
2024-01-29 15:42:52 -0600

    west setup

--- week-7/day-3/inheritance.js
+++ week-7/day-3/inheritance.js
@@ -1,13 +1,54 @@
 class Book {
+  constructor(title, series, author) {
+    this.title = title;
+    this.series = series;
+    this.author = author;
 
+    Book.count++;
+  }
+  static count = 0;
+
+  changeAuthor(newAuthor) {
+    this.author = newAuthor;
+    return `${this.title} by ${this.author}...`;
+  }
+  
+  static getCount() {
+    console.log(`There are ${Book.count} books...`);
+  }
 }
+
 
 class Stationary {
+  constructor(type, material, bodyColor, label) {
+    this.type = type;
+    this.material = material;
+    this.bodyColor = bodyColor;
+    this.label = label;
+  }
 
+  write(msg) {
+    console.log(msg);
+  }
 }
 
-class Pen {
+class Pen extends Stationary {
+  constructor(inkColor = 'black', inkLevel = 100) {
+    super('Pen', 'Plastic', 'White', 'bic');
+    this.inkColor = inkColor;
+    this.inkLevel = inkLevel;
+  }
 
+  write(msg) {
+    if (this.inkLevel < msg.length) {
+      console.log('Not Enough Ink!!!')
+    }
+    console.log(msg);
+    this.inkLevel -= msg.length;
+  }
+  refill() {
+    this.inkLevel = 100;
+  }
 }
 
 module.exports = {
--- week-7/day-3/scratch.js
+++ week-7/day-3/scratch.js
@@ -1,2 +1,25 @@
 const {Book, Stationary, Pen} = require('./inheritance.js');
 
+let im = new Book('Invisible Monsters', 'n/a', 'Chuck Palahniuk');
+let fc = new Book('Fight Club', 'n/a', 'Chuck Palahniuk')
+
+// console.log(im);
+// console.log(fc);
+// console.log(fc.changeAuthor('JRR Tolkien'));
+// fc.changeAuthor('Tristan Huckabee')
+// console.log(fc);
+
+// Book.getCount();
+// fc.getCount();
+
+let pen = new Pen();
+console.log(pen);
+pen.write('Hello!')
+pen.write('Hello!')
+pen.write('Hello!')
+pen.write('Hello!')
+pen.write('Hello!')
+pen.write('Hello!')
+pen.write('Hello!')
+
+console.log(pen.inkLevel);
