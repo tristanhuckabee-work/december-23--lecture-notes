ea4c269f8c571d4d5e4f6a151f7584c8c88c63f2
tristan <thuckabee.work@gmail.com>
2024-02-14 17:53:53 -0600

    east done

--- week-10/day-3/scratch.js
+++ week-10/day-3/scratch.js
@@ -1,7 +1,63 @@
 const sha256 = require('js-sha256');
 
-console.log(sha256('hello')) // 53
-console.log(sha256('hello')) // 53
+let data = [null, null, null, null, null, null, null, null, null, null];
 
-console.log(sha256('bill'))  // 41
-console.log(sha256('Bill'))  // 38
+function hash(str) {
+  let hashValue = 0;
+
+  for (let i = 0 ; i < str.length ; i++) {
+    hashValue += str.charCodeAt(i);
+  }
+
+  return hashValue;
+}
+function hashMod(key) {
+  return hash(key) % data.length;
+}
+data[hashMod('hello')] = 'hello';
+// console.log(data);
+
+class KeyValPair {
+  constructor(key, val) {
+    this.key = key;
+    this.value = val;
+    this.next = null;
+  }
+}
+class HashTable {
+  constructor() {
+    // this.data = [null, null, null, null, null, null, null, null, null, null];
+    this.data = new Array(10).fill(null);
+  }
+
+  addToHead(head, node) {
+    node.next = head;
+    return node;
+  }
+  addData(key, value) { //O(1)
+    let pair = new KeyValPair(key, value);
+    let mod = hashMod(key); // worst- O(n) Average-case O(1)
+
+    if (!this.data[mod]) {
+      this.data[mod] = pair;
+      return;
+    }
+    this.data[mod] = this.addToHead(this.data[mod], pair);
+  }
+  getData(key) {
+    let mod = hashMod(key);
+
+    return this.data[mod];
+  }
+}
+
+let htable = new HashTable();
+htable.addData('hello', 'hi');
+htable.addData('R', 'collided?')
+
+// console.log(htable.getData('hello'));
+console.log(htable.data);
+
+// console.log(sha256('superSecretPassword'));
+// console.log(sha256('asdflkasjdf;lkasdjf;lasdkjfas;dlkfjasd;lfk'));
+// console.log(sha256('super'));
